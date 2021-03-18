dd01e47527f0d27402e3bda8be9cb8219370e0a9
codie-arun <42159657+codie-arun@users.noreply.github.com>
2021-03-18 23:56:49 +0530

    Add files via upload

---
+++ jsExercises/FrameDemo.java
@@ -0,0 +1,15 @@
+package week5day4;
+
+import java.awt.Frame;
+
+public class FrameDemo {
+
+	public static void main(String[] args) {
+		
+		Frame frame = new Frame();
+		frame.setSize(400,500);
+		frame.setVisible(true);
+		System.out.println("frame created");
+	}
+
+}
